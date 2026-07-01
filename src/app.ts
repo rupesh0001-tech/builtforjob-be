a715615d7ffad797bf75495a431854171990c458
Rupesh Jagtap <rupeshcoding01@gmail.com>
2026-07-01 17:41:47 +0530

    added new origin

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3001",
   "https://build-for-job-fe.vercel.app", 
-  'http://buildforjob.rupeshhh.in'
+  'http://buildforjob.rupeshhh.in',
+  'https://buildforjob.rupeshhh.in',
 ];
 
 if (process.env.FRONTEND_URL) {
